c25d64f51db964581e7146da7c61af0d886d63f5
Eka Abashidze <eka.abashidze@redberry.ge>
2023-04-04 20:42:59 +0400

    chore: add language buttons

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -5,21 +5,29 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
   <title>Movie Quotes</title>
-    <!-- <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"> -->
 </head>
 <body>
 
-<div class="h-screen flex flex-col justify-center">
 
-  <div class="mx-auto text-center flex flex-col content-center justify-evenly items-center">
 
-      <img class="block max-w-700" src="{{ asset('image.png') }}" alt="Movie Scene">
-      <h1 class="text-3xl font-bold text-white mt-16 mb-28">“What should I tell you your mother?!”</h1>
-      <h1 class="text-3xl font-bold text-white underline font-roboto">The Son of Soldier</h1>
+    <div class="flex flex-col gap-y-[15px] fixed top-[50%] left-[50px] top-1/2 transform  -translate-y-1/2" >
+      <div class="border border-white w-[58px] h-[58px] rounded-full text-white flex items-center font-roboto justify-center hover:text-gray-dark hover:bg-white cursor-pointer">En</div>
+        <div class="border border-white w-[58px] h-[58px] rounded-full text-white flex items-center font-roboto justify-center hover:text-gray-dark hover:bg-white cursor-pointer">Ka</div>
+    </div>
 
-  </div>
 
-</div>
+    <div class="h-screen flex flex-col justify-center">
+
+      <div class="mx-auto text-center flex flex-col content-center justify-evenly items-center">
+
+          <img class="max-w-700 rounded-md" src="{{ asset('image.png') }}" alt="Movie Scene">
+          <h1 class="text-3xl font-bold text-white mt-16 mb-28">“What should I tell you your mother?!”</h1>
+          <h1 class="text-3xl font-bold text-white underline font-roboto">The Son of Soldier</h1>
+
+      </div>
+
+    </div>
+
 
 
 </body>
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -5,9 +5,17 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   @vite('resources/css/app.css')
   <title>Movie Quotes</title>
-    <!-- <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"> -->
 </head>
 <body>
+
+
+
+    <div class="flex flex-col gap-y-[15px] fixed top-[50%] left-[50px] top-1/2 transform  -translate-y-1/2" >
+      <div class="border border-white w-[58px] h-[58px] rounded-full text-white flex items-center font-roboto justify-center hover:text-gray-dark hover:bg-white cursor-pointer">En</div>
+      <div class="border border-white w-[58px] h-[58px] rounded-full text-white flex items-center font-roboto justify-center hover:text-gray-dark hover:bg-white cursor-pointer">Ka</div>
+    </div>
+
+
 
 <div class="h-screen flex flex-col justify-center">
 
@@ -26,7 +34,33 @@
 
   </div>
 
+
 </div>
+
+
+
+<!-- TODO SECOND CARD -->
+
+
+<div class="h-screen flex flex-col justify-center">
+
+  <div class="mx-auto flex flex-col content-center justify-evenly pt-[79px]">
+
+    <div class="w-[748px] h-[533px] bg-white flex flex-col items-center overflow-hidden rounded-md">
+
+      <img class="min-w-[100%] border border-black" src="{{ asset('image.png') }}" alt="Movie Scene">
+
+      <h1 class="text-3xl font-bold text-gray-dark mt-[32px]">“What should I tell you your mother?!”</h1>
+
+    </div>
+
+
+  </div>
+
+
+</div>
+
+
 
 
 </body>

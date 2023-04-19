79108973204172d7cbbf4443fb2d075cb9735981
Eka Abashidze <eka.abashidze@redberry.ge>
2023-04-19 18:51:53 +0400

    refactor: extract menu component

--- resources/views/editmovie.blade.php
+++ resources/views/editmovie.blade.php
@@ -11,26 +11,7 @@
 
 <div class="flex flex-row h-screen w-full">
     
-        <div class="flex flex-col bg-blue-dark text-white w-64">
-
-            <div class="p-8 text-3xl font-bold">{{ __('messages.admin_panel') }}</div>
-            <ul class="flex flex-col p-8">
-                <li class="py-2 hover:underline"><a href="#">{{ __('messages.main_page') }}</a></li>
-                <li class="py-2 hover:underline"><a href="#">{{ __('messages.create_movie') }}</a></li>
-                <li class="py-2 hover:underline"><a href="#">{{ __('messages.create_quote') }}</a></li>
-                <li class="py-2 hover:underline">
-                    <form method="POST" action="{{ route('logout') }}">
-                        @csrf
-                        <button type="submit">{{ __('messages.log_out') }}</button>
-                    </form>
-                </li>
-            </ul>
-            
-            <div class="flex flex-row justify-start items-center p-8">
-                <div class="border border-white w-10 h-10 rounded-full text-white flex items-center font-roboto justify-center hover:text-gray-dark hover:bg-white cursor-pointer">En</div>
-                <div class="border border-white w-10 h-10 rounded-full text-white flex items-center font-roboto justify-center hover:text-gray-dark hover:bg-white cursor-pointer ml-4">Ka</div>
-            </div>
-        </div>
+ <x-menu/>
 
     <div class="flex flex-col flex-1 bg-gray-100 p-8">
         <h2 class="text-2xl font-bold mb-8">{{ __('messages.edit_movie') }}</h2>
